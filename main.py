2f51ec92d742b7f469b6024bed765061c8fa999f
Joy <joynisha.sumpter@gmail.com>
2024-08-07 21:57:57 -0400

    Update main

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ topic_request = streamlit.text_input("Enter a topic for your post","How pretty w
 translation_request = streamlit.selectbox("What Language should the post be in?", ("EN", "ES"))
 streamlit.write(image_request)
 streamlit.write(topic_request)
-final_text = ""
+
 if streamlit.button("Submit", type="primary"):
     streamlit.write("We will send to Open AI Here and return the post in ",translation_request)
-    streamlit.text("What we returned from OpenAI")
+    openAI_results = streamlit.text("What we returned from OpenAI")
+    streamlit.session_state.key = openAI_results
 
-platform_request = streamlit.selectbox("Which platform do you want to post on?", ("LINKEDIN", "TWITTER"))
-if streamlit.button("Post", type="primary") and final_text != "":
-    streamlit.write("We Posted the content on ",platform_request)
+if 'key' in streamlit.session_state:
+    platform_request = streamlit.selectbox("Which platform do you want to post on?", ("LINKEDIN", "TWITTER"))
+    if streamlit.button("Post", type="primary"):
+        streamlit.write("We Posted the content on ",platform_request)
