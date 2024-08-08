8aa0b213ff3c5e53b403be62fcdb076f21e7ec8a
Joy <joynisha.sumpter@gmail.com>
2024-08-07 21:19:57 -0400

    Adding localization placeholder

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import streamlit
 
 image_request = streamlit.text_input("Enter text for the type of image you want","A cute white bunny with blue eyes")
 topic_request = streamlit.text_input("Enter a topic for your post","How pretty white bunnies with blue eyes are")
+translation_request = streamlit.selectbox("What Language should the post be in", ("EN", "ES"))
 streamlit.write(image_request)
 streamlit.write(topic_request)
 if streamlit.button("Submit", type="primary"):
-    streamlit.write("We will send to Open AI Here")
+    streamlit.write("We will send to Open AI Here and return the post in " + translation_request)
