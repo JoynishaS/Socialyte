dc2dccd3b007993235560d0636d4b31879115442
Joy <joynisha.sumpter@gmail.com>
2024-08-07 21:07:41 -0400

    Update main.py with button and more input fields

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import streamlit
 
 image_request = streamlit.text_input("Enter text for the type of image you want","A cute white bunny with blue eyes")
+topic_request = streamlit.text_input("Enter a topic for your post","How pretty white bunnies with blue eyes are")
 streamlit.write(image_request)
+streamlit.write(topic_request)
+streamlit.button("Submit", type="primary")
