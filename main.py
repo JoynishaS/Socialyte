9bceb8b8790f38b5eab86caefc0f50c405be068d
Joy <joynisha.sumpter@gmail.com>
2024-08-07 21:26:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import streamlit
 
 image_request = streamlit.text_input("Enter text for the type of image you want","A cute white bunny with blue eyes")
 topic_request = streamlit.text_input("Enter a topic for your post","How pretty white bunnies with blue eyes are")
-translation_request = streamlit.selectbox("What Language should the post be in", ("EN", "ES"))
+translation_request = streamlit.selectbox("What Language should the post be in?", ("EN", "ES"))
 streamlit.write(image_request)
 streamlit.write(topic_request)
 if streamlit.button("Submit", type="primary"):
     streamlit.write("We will send to Open AI Here and return the post in " + translation_request)
+    platform_request = streamlit.selectbox("Which platform do you want to post on?", ("LINKEDIN", "TWITTER"))
+    streamlit.button("Post", type="primary")
