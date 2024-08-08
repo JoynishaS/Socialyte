00616bae2cc4008f0fd6eece2c94a78c64bb148a
Joy <joynisha.sumpter@gmail.com>
2024-08-07 20:53:23 -0400

    Updating to add text input

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import streamlit
 
-streamlit.write("hello world");
+image_request = streamlit.text_input("Enter text for the type of image you want","A cute white bunny with blue eyes")
+streamlit.write(image_request)
