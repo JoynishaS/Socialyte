b62989f82d017c68cb7cb4d26652b775f44012e1
Joy <joynisha.sumpter@gmail.com>
2024-08-11 11:01:45 -0400

    Adding Secret Key for Granite Access Token and function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit
 from openai import OpenAI
+import requests
 
 #Open AI Client Authorization
 client = OpenAI(
@@ -7,29 +8,6 @@ client = OpenAI(
     api_key = streamlit.secrets['OPENAI_API_KEY'],
     project='proj_fqZ8kTAr98ZSgzH5cEdb8mGW'
 )
-
-#Radio Buttons For Image Generation Options
-imageModificationChoice = streamlit.radio(
-    "Generate an image, Upload your own image",
-    ["***Generate Image by Text***", "***Upload your own Image***"],
-    index=None,
-)
-
-#Switch Statement for Image Generation Options
-match imageModificationChoice:
-    case "***Generate Image by Text***":
-        image_request = streamlit.text_input("Enter text for the type of image you want","A cute white bunny with blue eyes")
-        streamlit.session_state['image_request'] = image_request
-    case "***Upload your own Image***":
-        uploaded_file = streamlit.file_uploader("Choose a 1024x1024 png image",type = ['png'])
-        if uploaded_file is not None:
-            streamlit.session_state['image'] = uploaded_file
-
-#Input Field for User to enter their request to Open AI for Text Generation
-topic_request = streamlit.text_input("Enter a topic for your post","Write a post for Twitter about the history of White Bunnies with Blue Eyes")
-
-#Dropdown to allow users to choose what language they want to translate to
-translation_request = streamlit.selectbox("What Language should the post be in?", ("EN", "ES"))
 
 #Function to send user request for images to Open AI
 def sendToOpenAI(description):
@@ -55,6 +33,68 @@ def sendTextToOpenAI(userRequest):
             {"role": "user", "content": userRequest}
                 ]
     )
+
+#Function to return localized from Granite
+def graniteTextLocalization(texttoTranslate):
+    url = "https://us-south.ml.cloud.ibm.com/ml/v1/text/generation?version=2023-05-29"
+    body = {
+        "input": """Translate the following text from English to Spanish: 
+        Text:%s
+        Translation:"""%texttoTranslate,
+            "parameters": {
+                "decoding_method": "sample",
+                "max_new_tokens": 1024,
+                "min_new_tokens": 1,
+                "random_seed": 42,
+                "stop_sequences": ["\n"],
+                "temperature": 0.5,
+                "top_k": 50,
+                "top_p": 0.75,
+                "repetition_penalty": 1
+            },
+            "model_id": "ibm/granite-20b-multilingual",
+            "project_id": "7fd2eccd-c7d6-4d4b-a0d9-0649afd36f75"
+    }
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "%s"%streamlit.secrets['GRANITE_ACCESS_TOKEN']
+    }
+
+    response = requests.post(
+        url,
+        headers=headers,
+        json=body
+    )
+
+    if response.status_code != 200:
+        raise Exception("Non-200 response: " + str(response.text))
+
+    data = response.json()
+    return data
+
+#Radio Buttons For Image Generation Options
+imageModificationChoice = streamlit.radio(
+    "Generate an image, Upload your own image",
+    ["***Generate Image by Text***", "***Upload your own Image***"],
+    index=None,
+)
+
+#Switch Statement for Image Generation Options
+match imageModificationChoice:
+    case "***Generate Image by Text***":
+        image_request = streamlit.text_input("Enter text for the type of image you want","A cute white bunny with blue eyes")
+        streamlit.session_state['image_request'] = image_request
+    case "***Upload your own Image***":
+        uploaded_file = streamlit.file_uploader("Choose a 1024x1024 png image",type = ['png'])
+        if uploaded_file is not None:
+            streamlit.session_state['image'] = uploaded_file
+
+#Input Field for User to enter their request to Open AI for Text Generation
+topic_request = streamlit.text_input("Enter a topic for your post","Write a post for Twitter about the history of White Bunnies with Blue Eyes")
+
+#Dropdown to allow users to choose what language they want to translate to
+translation_request = streamlit.selectbox("What Language should the post be in?", ("EN", "ES"))
 
 #When the user clicks submit add the text returned and image from Open AI to the screen, also save text for use later.
 if streamlit.button("Submit", type="primary"):
