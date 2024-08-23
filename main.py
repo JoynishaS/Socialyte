e916c2f8d5ab6a8125ed41b61d77c11fa85bab91
Joy <joynisha.sumpter@gmail.com>
2024-08-23 00:22:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ def requestTwitterToken():
         streamlit.session_state['oauth_token_secret'] = fetch_response.get("oauth_token_secret")
         streamlit.write("Fetch Token")
         streamlit.write("Got OAuth token: %s" % streamlit.session_state['oauth_token'])
+        streamlit.write("Got OAuth token: %s" % streamlit.session_state['oauth_token_secret'])
     except ValueError:
         streamlit.write("There may have been an issue with the consumer_key or consumer_secret you entered.")
 
