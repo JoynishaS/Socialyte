8cce61c6cca32056c93a31a898435a9fe12adaac
Joy <joynisha.sumpter@gmail.com>
2024-08-23 15:16:02 -0400

    Update Twitterapi

--- TwitterApi.py
+++ TwitterApi.py
@@ -25,7 +25,7 @@ def postToTwitter():
     imageID = uploadImage()
     streamlit.write(imageID)
     payload = {"text": streamlit.session_state['key'],
-               "media": {"media_ids": [imageID["media_id"]]}
+               "media": {"media_ids": [str(imageID["media_id"])]}
                }
 
     # Make the request
