cce386c87c56f0e3c01b6b38632c0053b11b3cf9
Joy <joynisha.sumpter@gmail.com>
2024-08-23 15:09:42 -0400

    Trying to see what the problem is

--- TwitterApi.py
+++ TwitterApi.py
@@ -41,17 +41,8 @@ def postToTwitter():
         "https://api.twitter.com/2/tweets",
         json=payload,
     )
-
-    if response.status_code != 201:
-        raise Exception(
-            "Request returned an error: {} {}".format(response.status_code, response.text)
-        )
-
-    print("Response code: {}".format(response.status_code))
-
-    # Saving the response as JSON
-    json_response = response.json()
-    print(json.dumps(json_response, indent=4, sort_keys=True))
+    streamlit.write(response.status_code)
+    streamlit.write(response.text)
 
 #Upload Image to twitter
 def uploadImage():
